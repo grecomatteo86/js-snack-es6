edf056b09bfa1537a4665cd0d2f74c7c56dc3c16
Matteo <grecomatteo86@gmail.com>
2021-03-19 16:42:03 +0100

    Snack_2

--- Snack_2/js/script.js
+++ Snack_2/js/script.js
@@ -1,33 +1,68 @@
-/* Creare un array di oggetti:
-ogni oggetto descriverà una bici da corsa
-con le seguenti proprietà: nome e peso.
-Stampare a schermo la bici con peso minore */
+/* Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare,
+le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà:
+Punti fatti e falli subiti.
+Infine usando la destrutturazione creiamo un nuovo array
+i cui elementi contengono solo nomi e falli subiti
+e stampiamo tutto in console */
 
-const arrayBici = [
+/* Creo un array di oggetti di squadre di calcio */
+const arreySquadre = [
   {
-    nome:'Atala',
-    peso: '2500',
+    nome:'Palermo',
+    puntiFatti:'0',
+    falliSubiti:'0'
   },
   {
-    nome:'Bianchi',
-    peso: '2200',
+    nome:'Catania',
+    puntiFatti:'0',
+    falliSubiti:'0'
   },
   {
-    nome:'Girardengo',
-    peso: '2100',
+    nome:'Messina',
+    puntiFatti:'0',
+    falliSubiti:'0'
   }
 ];
 
-var pesoRiferimento = arrayBici[0].peso;
+/* Creo la mia funzione randomizzatore */
+function randomizzatore (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
-for ( let i = 0; i < arrayBici.length; i++ ) {
+/* Cambio i valori assegnando numeri random
+al posto di Punti fatti e falli subiti */
 
-  if ( arrayBici[i].peso < pesoRiferimento) {
+for (let i = 0; i < arreySquadre.length; i++) {
 
-    pesoRiferimento = arrayBici[i].peso;
+  arreySquadre[i].puntiFatti = randomizzatore (0, 10);
 
-  }
+  arreySquadre[i].falliSubiti = randomizzatore (0, 10);
 
 }
 
-console.log(pesoRiferimento);
+console.log(arreySquadre);
+
+
+/* Uso la destrutturazione per creare un nuovo array
+i cui elementi contengono solo nomi e falli subiti
+e stampo tutto in console */
+
+const nuovoArreySquadre = [];
+
+for (var i = 0; i < arreySquadre.length; i++) {
+
+  var obj = {
+
+    nome: arreySquadre[i].nome,
+    falliSubiti: arreySquadre[i].falliSubiti
+
+  };
+
+  nuovoArreySquadre.push(obj);
+
+}
+
+console.log(nuovoArreySquadre);
