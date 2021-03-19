c6484d480be9feed883c562e9a44371b798a875e
Matteo <grecomatteo86@gmail.com>
2021-03-19 15:49:59 +0100

    Snack_1

---
+++ Snack_2/js/script.js
@@ -0,0 +1,33 @@
+/* Creare un array di oggetti:
+ogni oggetto descriverà una bici da corsa
+con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore */
+
+const arrayBici = [
+  {
+    nome:'Atala',
+    peso: '2500',
+  },
+  {
+    nome:'Bianchi',
+    peso: '2200',
+  },
+  {
+    nome:'Girardengo',
+    peso: '2100',
+  }
+];
+
+var pesoRiferimento = arrayBici[0].peso;
+
+for ( let i = 0; i < arrayBici.length; i++ ) {
+
+  if ( arrayBici[i].peso < pesoRiferimento) {
+
+    pesoRiferimento = arrayBici[i].peso;
+
+  }
+
+}
+
+console.log(pesoRiferimento);
