af787b4a575c9a2a7ab4a4f0f5e40cb3827bfafb
Matteo <grecomatteo86@gmail.com>
2021-03-22 10:18:18 +0100

    fixes

--- Snack_3/js/script.js
+++ Snack_3/js/script.js
@@ -3,18 +3,21 @@ un array e due numeri (a più piccolo di b).
 La funzione ritornerà un nuovo array con i valori
 che hanno la posizione compresa tra i due numeri */
 
-/* 1. Mi creo un array di dieci numeri e un array vuoto */
-var array = [1,2,3,4,5,6,7,8,9,10];
-var newArray = [];
-/* 2. Mi creo una funzione a cui gli passo un array e due numeri;
+/* 1. Mi creo una funzione a cui gli passo un array e due numeri;
 questa funzione mi cicla l'array prendendo come estremi i numeri che gli passo
 e me fa il push in un nuovo array */
 function myFunction (array,a,b) {
+  const newArray = [];
+
   for (let i = a; i <= b; i++) {
     newArray.push(i);
   }
+
+  return newArray;
 }
-/* 3. Evoco la funzione e gli passo i paramentri che mi interressano */
-myFunction (array,6,8);
-/* 4. Stampo l'array precedentemente vuoto che adesso ha i numeri pushati */
-console.log(newArray);
+/* 2. Mi creo un array di dieci numeri */
+const array = [1,2,3,4,5,6,7,8,9,10];
+/* 3. Evoco la funzione per assegnare i valori che gli passo in un secondo array */
+const arrayDue = myFunction (array,2,5);
+/* 4. Stampo il secondo array */
+console.log(arrayDue);
